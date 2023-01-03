98a4dac85fea282430f636895cf55ab33fc9b9d7
Sawantaviraj2 <121309811+Sawantaviraj2@users.noreply.github.com>
2023-01-03 15:19:30 +0530

    Prime Number Problem

---
+++ prime.js
@@ -0,0 +1,17 @@
+let n=3;
+let c=0;
+for(let i=1;i<=3;i++)
+{
+   if(3%i==0)
+   {
+      c++;
+   }
+}
+if(c==2)
+{
+  console.log("Prime Number");
+}
+else
+{
+   console.log("Not a prime number");
+}
